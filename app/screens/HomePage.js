1d53f1c5288187dc4363af93fa595af07f850274
eheka78 <gemddkim22@gmail.com>
2025-11-17 15:39:46 +0900

    feat: 홈,룰렛 토글 커스텀

--- app/screens/HomePage.js
+++ app/screens/HomePage.js
@@ -1,27 +1,32 @@
 import React, { useState } from 'react';
-import { Button, Text, View, Image } from 'react-native';
+import { Button, Text, View, Image, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Home from './Home.js';
 import Roulette from './Roulette.js';
+import Toggle from '../components/Toggle.js';
 
 export default function HomePage({ navigation }) {
-    const [showRoulette, setShowRoulette] = useState(false);  // false: 홈, true: 룰렛
+    const [homeType, setHomeType] = useState("홈");  // false: 홈, true: 룰렛
 
     return (
         <SafeAreaView style={{ backgroundColor:"white"}}> 
             {/* 홈, 룰렛 토글 버튼 */}
-            <Button
-                title={showRoulette ? "홈" : "룰렛"}
-                onPress={() => setShowRoulette(prev => !prev)}
+            <Toggle 
+                leftLabel="홈"
+                rightLabel="룰렛"
+                onToggle={(label) => setHomeType(label)} 
             />
 
-            {/* 홈, 룰렛 페이지 import */}
-            {showRoulette ? <Home /> : <Roulette />}
-            
-            <Button
-                title="로그인 페이지로 이동"
-                onPress={() => navigation.getParent().navigate("Login")}
-            />
+            <ScrollView>
+                {/* 홈, 룰렛 페이지 import */}
+                {homeType === "홈" && <Home />}
+                {homeType === "룰렛" && <Roulette />}
+                
+                <Button
+                    title="로그인 페이지로 이동"
+                    onPress={() => navigation.getParent().navigate("Login")}
+                />
+            </ScrollView>
         </SafeAreaView>
     );
 }
