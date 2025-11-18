e2e9eb23cef9c9aced36fa9cb7535047f4081aaf
eheka78 <gemddkim22@gmail.com>
2025-11-18 15:35:45 +0900

    refactor: CalendarForm 컴포넌트명을 Calendar로 변경

--- app/screens/MealLog.js
+++ app/screens/MealLog.js
@@ -1,6 +1,6 @@
 import { Button, Text, View } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import CalendarForm from '../components/Calendar';
+import Calendar from '../components/Calendar';
 
 export default function MealLog({ navigation }) {
 	return (
@@ -8,7 +8,7 @@ export default function MealLog({ navigation }) {
 			<SafeAreaView style={{ flex: 1, backgroundColor:"white" }}> 
 				<View>
 					<Text>MealLog</Text>
-					<CalendarForm></CalendarForm>
+					<Calendar></Calendar>
 				</View>
 			</SafeAreaView>
 		</SafeAreaProvider>
