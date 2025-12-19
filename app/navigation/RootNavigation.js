ff58ec537eb03b4cd0abd078462f8a5e3ccb7554
eheka78 <gemddkim22@gmail.com>
2025-12-19 20:40:00 +0900

    feat: RootNavigation 생성

---
+++ app/navigation/RootNavigation.js
@@ -0,0 +1,13 @@
+// navigation/RootNavigation.js
+import { createNavigationContainerRef } from '@react-navigation/native';
+
+export const navigationRef = createNavigationContainerRef();
+
+export function resetToLogin() {
+  if (navigationRef.isReady()) {
+    navigationRef.reset({
+      index: 0,
+      routes: [{ name: 'Login' }],
+    });
+  }
+}
