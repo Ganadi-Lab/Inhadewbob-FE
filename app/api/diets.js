da7d49d1adf382e75f5e36b10b280da32a69d495
eheka78 <gemddkim22@gmail.com>
2025-12-19 20:38:11 +0900

    refactor: 코드 스타일 변경

--- app/api/diets.js
+++ app/api/diets.js
@@ -2,23 +2,37 @@ import { formatDateTime } from "../utils/FormatDateTime";
 import api from "./api";
 
 export const createDiet = async ({ date, time, restaurantName, menuName, price }) => {
-  try {
-    const payload = {
-      date: date,
-      time: time,
-      restaurantName,
-      menuName,
-      price: Number(price),
-    };
+    try {
+        const payload = {
+            date: date,
+            time: time,
+            restaurantName,
+            menuName,
+            price: Number(price),
+        };
 
-    console.log(' /diets 연결 api', payload);
+        console.log(' /diets 연결 api', payload);
 
-    const res = await api.post('/diets', payload);
+        const res = await api.post('/diets', payload);
 
-    console.log('diets api 연결 성공', res.status, res.data);
-    return res.data;
-  } catch (e) {
-    console.log('diets api 연결 실패', e?.response?.status, e?.response?.data, e?.message);
-    throw e;
-  }
+        console.log('diets api 연결 성공', res.status, res.data);
+        return res.data;
+    } catch (e) {
+        console.log('diets api 연결 실패', e?.response?.status, e?.response?.data, e?.message);
+        throw e;
+    }
+};
+
+
+export const deleteDiet = async (id) => {
+    console.log(id);
+    try {
+        const res = await api.delete(`/diets/${id}`);
+
+        console.log('diets api 연결 성공', res.status, res.data);
+        return res.data;
+    } catch (e) {
+        console.log('diets api 연결 실패', e?.response?.status, e?.response?.data, e?.message);
+        throw e;
+    }
 };
